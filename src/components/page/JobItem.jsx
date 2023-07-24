a171c55810be5edd9c745773e19d669b6d633fe4
Niloy Chakravorty <chakravortyniloy@gmail.com>
2023-07-24 22:36:10 +0530

    added view for each post

--- src/components/page/JobItem.jsx
+++ src/components/page/JobItem.jsx
@@ -1,9 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import  { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const JobItem = () => {
     let {jobId} = useParams();
     const [job, setJob] = useState(null);
+    const handleDelete = () => {
+
+    }
+    const handleUpdate = () => {
+      
+    }
     useEffect(() => {
         fetch(`http://localhost:8080/api/post/${jobId}`, {
             method: 'GET',
@@ -19,9 +25,35 @@ const JobItem = () => {
             });
         }, []);
   return (
-    <div>
-      
+    
+       <div className="d-flex justify-content-center align-items-center" style={{ minHeight: 'calc(100vh - 56px)', backgroundColor: '#eeeeee' }}>
+      <div className="container p-4">
+        {job ? (
+          <div>
+            <h5 className="text-center mb-4">{job.profile}</h5>
+            <div className="card mx-auto" style={{ width: 'calc(100% + 20px)', backgroundColor: '#eeeeee', borderRadius: '0.25rem', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)' }}>
+              <div className="card-body" >
+                <h6 style={{fontSize: '19px', color: '#6B7171',}}>Description :</h6>
+                <p className="card-text" style={{fontSize: '15px'}}>{job.desc}</p>
+                
+                <center>
+                  <p className="card-text" style={{ color: '#154c79', fontWeight: 'bold' }}>{job.techStack}</p>
+                  <p className="card-text" style={{ color: '#404D4D', fontWeight: 'bold' }}>{job.location}</p>
+                </center>
+              </div>
+            </div>
+            <br></br>
+            <center>
+              <button type="button" className="btn btn-info mx-3" onClick={handleUpdate}>Update</button>
+              <button type="button" className="btn btn-warning" onClick={handleDelete}>Delete</button>
+            </center>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </div>
+    
   )
 }
 
